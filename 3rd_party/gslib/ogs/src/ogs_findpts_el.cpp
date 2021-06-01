f83df1aa51cde3e0ff5e7dec93b0d1944f6650c1
Neil Lindquist <neillindquist5@gmail.com>
2021-06-01 16:10:50 -0500

    More fixes

--- 3rd_party/gslib/ogs/src/ogs_findpts_el.cpp
+++ 3rd_party/gslib/ogs/src/ogs_findpts_el.cpp
@@ -8,6 +8,7 @@
 #include "ogsKernels.hpp"
 #include "name.h"
 #include "poly.h"
+#include "findpts_el.h"
 #include "platform.hpp"
 #include "ogs_findpts_el.h"
 
@@ -21,21 +22,26 @@ void ogs_findpts_el_eval_2(
   const unsigned nr=fd->n[0],ns=fd->n[1];
   occa::device device = platform->device;
 
+  assert(out_stride % sizeof(double) == 0);
+  const unsigned d_out_stride = out_stride / sizeof(double);
+  assert(r_stride % sizeof(double) == 0);
+  const unsigned d_r_stride = r_stride / sizeof(double);
+
   occa::memory d_out_base = device.malloc(out_stride*pn);
   occa::memory   d_r_base = device.malloc(  r_stride*pn);
   d_r_base.copyFrom(r_base);
   occa::memory d_in = device.malloc(nr*ns*sizeof(double));
   d_in.copyFrom(in);
 
-  occa::memory d_lag_data_0 = device.malloc(nr*sizeof(double));
+  occa::memory d_lag_data_0 = device.malloc(gll_lag_size(nr)*sizeof(double));
   d_lag_data_0.copyFrom(fd->lag_data[0]);
-  occa::memory d_lag_data_1 = device.malloc(ns*sizeof(double));
+  occa::memory d_lag_data_1 = device.malloc(gll_lag_size(ns)*sizeof(double));
   d_lag_data_1.copyFrom(fd->lag_data[1]);
 
-  ogs::findpts_el_eval_2(d_out_base, out_stride,
-                      d_r_base,   r_stride,
-                    pn, d_in,
-                    nr, ns, d_lag_data_0, d_lag_data_1);
+  ogs::findpts_el_eval_2(d_out_base, d_out_stride,
+                           d_r_base,   d_r_stride,
+                         pn, d_in,
+                         nr, ns, d_lag_data_0, d_lag_data_1);
 
   d_out_base.copyTo(out_base);
 }
@@ -61,11 +67,11 @@ void ogs_findpts_el_eval_3(
   occa::memory d_in = device.malloc(nr*ns*nt*sizeof(double));
   d_in.copyFrom(in);
 
-  occa::memory d_lag_data_0 = device.malloc(nr*sizeof(double));
+  occa::memory d_lag_data_0 = device.malloc(gll_lag_size(nr)*sizeof(double));
   d_lag_data_0.copyFrom(fd->lag_data[0]);
-  occa::memory d_lag_data_1 = device.malloc(ns*sizeof(double));
+  occa::memory d_lag_data_1 = device.malloc(gll_lag_size(ns)*sizeof(double));
   d_lag_data_1.copyFrom(fd->lag_data[1]);
-  occa::memory d_lag_data_2 = device.malloc(nt*sizeof(double));
+  occa::memory d_lag_data_2 = device.malloc(gll_lag_size(nt)*sizeof(double));
   d_lag_data_2.copyFrom(fd->lag_data[2]);
 
   ogs::findpts_el_eval_3(d_out_base, d_out_stride,
