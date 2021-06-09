2717bb15fa0b54e200c0dda97f2cc7b851dd4663
Neil Lindquist <neillindquist5@gmail.com>
2021-06-09 15:35:12 -0500

    Improve the lifecycle management of findpts kernel

--- 3rd_party/gslib/ogs/ogs.hpp
+++ 3rd_party/gslib/ogs/ogs.hpp
@@ -238,7 +238,7 @@ typedef struct {
   int D;
   void *findpts_data;
   occa::device *device;
-  occa::kernel *local_eval_kernel;
+  occa::kernel local_eval_kernel;
   occa::memory lag_data[3];
 } ogs_findpts_t;
 
